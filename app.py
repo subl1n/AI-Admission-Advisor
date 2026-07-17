41ffed8179204871860e528db285a16917e3f6ad
Аман <amandiamond858@gmail.com>
2026-07-17 13:55:20 +0600

    Add application form and JSON storage

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+import json
+from fastapi import FastAPI, Form
 from fastapi.responses import FileResponse
 
 app = FastAPI()
@@ -8,6 +9,7 @@ app = FastAPI()
 def home():
     return FileResponse("templates/index.html")
 
+
 @app.get("/history")
 def history():
     return FileResponse("templates/history.html")
@@ -16,3 +18,32 @@ def history():
 @app.get("/about")
 def about():
     return FileResponse("templates/about.html")
+
+
+@app.post("/analyze")
+def analyze(
+    gpa: str = Form(...),
+    ielts: str = Form(...),
+    sat: str = Form(...),
+    portfolio: str = Form(...),
+    universities: str = Form(...)
+):
+    data = {
+        "gpa": gpa,
+        "ielts": ielts,
+        "sat": sat,
+        "portfolio": portfolio,
+        "universities": universities
+    }
+
+    with open("db.json", "r") as file:
+        database = json.load(file)
+
+    database.append(data)
+
+    with open("db.json", "w") as file:
+        json.dump(database, file, indent=4)
+
+    return {
+        "message": "Saved successfully!"
+    }
